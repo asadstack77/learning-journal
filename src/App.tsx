40c6805f2e45688050349a96d93b0ca77aaf89c1
asad-riaz611 <riazasad611@gmail.com>
2026-04-06 04:37:17 +0100

    expense-tracker connected to firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,91 @@
-import { useState } from "react";
+// src/App.tsx
+import { useState, useEffect } from "react";
+import axios from "axios";
 import ExpenseList from "./components/ExpenseTracker/ExpenseList";
 import ExpenseFilter from "./components/ExpenseTracker/ExpenseFilter";
 import ExpenseForm from "./components/ExpenseTracker/ExpenseForm";
 
+// Firebase REST API URL for your project (replace with your project ID)
+const FIREBASE_URL =
+  "https://expense-tracker-dev-asad-default-rtdb.firebaseio.com/expenses";
+
 const App = () => {
-  const [expenses, setExpense] = useState([
-    { id: 1, description: "Milk", amount: 20, category: "Groceries" },
-    { id: 2, description: "Bread", amount: 30, category: "Groceries" },
-    { id: 3, description: "Egg", amount: 70, category: "Groceries" },
-    { id: 4, description: "Electric Bill", amount: 70, category: "Utilities" },
-    { id: 5, description: "Guitar", amount: 70, category: "Entertainment" },
-  ]);
+  const [expenses, setExpenses] = useState<any[]>([]);
   const [category, setCategory] = useState("");
+
+  // Fetch expenses from Firebase
+  useEffect(() => {
+    axios
+      .get(`${FIREBASE_URL}.json`) // Firebase Realtime DB REST API
+      .then((res) => {
+        if (res.data) {
+          const data = Object.keys(res.data).map((key) => ({
+            id: key,
+            ...res.data[key],
+          }));
+          setExpenses(data);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  // Filter expenses
   const filteredExpenseList = category
     ? expenses.filter((e) => e.category === category)
     : expenses;
+
+  // Remove expense
   const handleRemove = (id: number) => {
-    setExpense(expenses.filter((e) => e.id !== id));
+    axios
+      .delete(`${FIREBASE_URL}/${id}.json`)
+      .then(() => {
+        setExpenses(expenses.filter((e) => e.id !== id));
+      })
+      .catch((err) => console.log(err));
   };
+
+  // Add new expense
   const handleSubmit = (data: any) => {
-    setExpense([...expenses, { ...data, id: expenses.length + 1 }]);
+    axios
+      .post(`${FIREBASE_URL}.json`, data)
+      .then((res) => {
+        setExpenses([...expenses, { ...data, id: res.data.name }]);
+      })
+      .catch((err) => console.log(err));
   };
+
   return (
-    <>
-      <div className="container py-3">
-        <div className="row g-3">
-          {/* Form */}
-          <div className="col-12 col-md-6">
-            <h5>ADD A NEW EXPENSE</h5>
-            <div className="p-3 border rounded bg-light">
-              <ExpenseForm onSubmitData={handleSubmit} />
-            </div>
+    <div className="container py-3">
+      <div className="row g-3">
+        {/* Form */}
+        <div className="col-12 col-md-6">
+          <h5>ADD A NEW EXPENSE</h5>
+          <div className="p-3 border rounded bg-light">
+            <ExpenseForm onSubmitData={handleSubmit} />
           </div>
+        </div>
 
-          {/* Filter */}
-          <div className="col-12 col-md-6">
-            <h5>FILTER BY CATEGORY</h5>
-            <div className="p-3 border rounded bg-light">
-              <ExpenseFilter
-                onSelectCategory={(category) => setCategory(category)}
-              />
-            </div>
+        {/* Filter */}
+        <div className="col-12 col-md-6">
+          <h5>FILTER BY CATEGORY</h5>
+          <div className="p-3 border rounded bg-light">
+            <ExpenseFilter
+              onSelectCategory={(category) => setCategory(category)}
+            />
           </div>
+        </div>
 
-          {/* List */}
-          <div className="col-12">
-            <div className="p-3 border rounded">
-              <ExpenseList
-                onRemove={handleRemove}
-                expense={filteredExpenseList}
-              />
-            </div>
+        {/* List */}
+        <div className="col-12">
+          <div className="p-3 border rounded table-responsive">
+            <ExpenseList
+              onRemove={handleRemove}
+              expense={filteredExpenseList}
+            />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChCpM-k1iarww7hY24dK4C2ZDrMyty6yk",
+  authDomain: "expense-tracker-dev-asad.firebaseapp.com",
+  projectId: "expense-tracker-dev-asad",
+  storageBucket: "expense-tracker-dev-asad.firebasestorage.app",
+  messagingSenderId: "316547284948",
+  appId: "1:316547284948:web:81d6d2992a5da0e18d6041"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
